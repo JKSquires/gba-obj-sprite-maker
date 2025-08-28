d968965f03be94144ef38d63d168f185f7e1d96a
JKSquires <168765098+JKSquires@users.noreply.github.com>
2025-08-27 21:15:21 -0500

    Add selected palette color indicator

--- scripts.js
+++ scripts.js
@@ -22,22 +22,34 @@ function col24bToCol15b(col_24b) {
 }
 
 function updateColorButtonTextColor(button) {
-	let r = button.dataset.color24b.substring(0,2);
-	let g = button.dataset.color24b.substring(2,4);
-	let b = button.dataset.color24b.substring(4,6);
-	//let g = button.style.backgroundColor.split(',')[1].replace(' ', '');
+	let r = parseInt(button.dataset.color24b.substring(0,2));
+	let g = parseInt(button.dataset.color24b.substring(2,4));
+	let b = parseInt(button.dataset.color24b.substring(4,6));
+	
 	let lum = 0.3 * r + 0.6 * g + 0.1 * b;
 
-	button.style.color = parseInt(lum) < 0x7F ? "#fff" : "#000";
+	button.style.color = lum < 0x7F ? "#fff" : "#000";
+}
+
+function updatePaletteColorSelection(col_num) {
+	let button = document.getElementById("col_" + col_num);
+
+	pal_col_sel.innerHTML = col_num;
+	pal_col_sel.style.backgroundColor = '#' + button.dataset.color24b;
+	pal_col_sel.style.color = button.style.color;
 }
 
 function savePaletteDialog() {
-	let button = document.getElementById("col_" + pal_col_id.innerHTML);
+	let col_num = pal_col_id.innerHTML;
+	let button = document.getElementById("col_" + col_num);
 	let color = pal_col_picker.value.substring(1);
+
 	button.style.backgroundColor = '#' + color;
 	button.dataset.color15b = col24bToCol15b(parseInt(color, 16)).toString(16).padStart(4, '0');
 	button.dataset.color24b = color;
 	updateColorButtonTextColor(button);
+
+	updatePaletteColorSelection(col_num);
 
 	palette_dialog.close();
 }
@@ -46,17 +58,19 @@ function colorButtonFunc(col_num) {
 	if (edit_palette) {
 		console.log("Edit " + col_num);
 		
-		let button = document.getElementById("col_0x" + col_num.toString(16));
+		let button = document.getElementById("col_" + col_num);
 
 		color_conv_box.value = "";
 		pal_col_picker.value = '#' + button.dataset.color24b;
 		pal_col_code.value = button.dataset.color15b;
 
-		pal_col_id.innerHTML = "0x" + col_num.toString(16);
+		pal_col_id.innerHTML = col_num;
 
 		palette_dialog.showModal();
 	} else {
 		console.log("Use " + col_num);
+		
+		updatePaletteColorSelection(col_num);
 	}
 }
 
@@ -68,19 +82,23 @@ function loadPaletteButtons() {
 	for (let col_index = 0; col_index < col_num; col_index++) {
 		let hex_index = "0x" + col_index.toString(16);
 		let col_button = document.createElement("button");
+
 		col_button.className = "pal_col_btn";
 		col_button.id = "col_" + hex_index;
 		col_button.innerHTML = hex_index;
-		col_button.onclick = () => colorButtonFunc(col_index);
 		col_button.style.backgroundColor = "#000";
 		
 		col_button.dataset.color15b = "0000";
 		col_button.dataset.color24b = "000000";
 		
+		col_button.onclick = () => colorButtonFunc(hex_index);
+
 		updateColorButtonTextColor(col_button);
 
 		palette_div.appendChild(col_button);
 	}
+	
+	updatePaletteColorSelection("0x0");
 }
 
 function btn15bTo24b() {
